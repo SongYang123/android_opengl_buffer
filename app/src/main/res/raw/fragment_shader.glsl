958c9414749e4a95720d7dfedaac7c7ef5cda447
Yang Song <ysong@zendesk.com>
2016-01-02 19:56:42 +0800

    add touch

--- app/src/main/res/raw/fragment_shader.glsl
+++ app/src/main/res/raw/fragment_shader.glsl
@@ -11,17 +11,14 @@ const float cDiffuse = 0.5;
 const vec4 cSpecularColor = vec4(1.0, 1.0, 1.0, 1.0);
 
 void main() {
-	vec3 mLightDir = normalize(uLightPos - vPosition);
-	vec3 mNormal = normalize(vNormal);
+	vec3 mLightDir = normalize(normalize(uLightPos - vPosition));
+	vec3 mNormal = normalize(normalize(vNormal));
 	float mDiffuse = max(dot(mNormal, mLightDir), 0.0);
 	float mSpecular = 0.0;
 	if (mDiffuse > 0.0) {
-		vec3 mViewDir = normalize(-vPosition);
-		vec3 mHalfDir = normalize(mLightDir + mViewDir);
+		vec3 mViewDir = normalize(normalize(-vPosition));
+		vec3 mHalfDir = normalize(normalize(mLightDir + mViewDir));
 		mSpecular = pow(max(dot(mHalfDir, mNormal), 0.0), 16.0);
 	}
-//	float mDist = length(uLightPos - vPosition);
-//	float mAttenuation = 1.0 / (1.0 + mDist * mDist * 0.25);
-//	gl_FragColor = uColor * (cAmbient + cDiffuse * mDiffuse * mAttenuation) + cSpecularColor * mSpecular;
 	gl_FragColor = uColor * (cAmbient + cDiffuse * mDiffuse) + cSpecularColor * mSpecular;
 }
